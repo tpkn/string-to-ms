13a042e1d2f407d6868b3637f1a30723d4382434
tpkn <tpkn@users.noreply.github.com>
2017-12-13 00:02:57 +0300

    Add files via upload

--- string-to-ms.js
+++ string-to-ms.js
@@ -3,33 +3,31 @@
  */
 
 module.exports = str => {
-   let summ = 0;
-   let time_type, time_value;
+   let summ = 0, time, type, value;
    let time_list = ('' + str).split(' ').filter(v => v != '' && /^(\d{1,}\.)?\d{1,}([wdhms])?$/i.test(v));
 
    for(let i = 0, len = time_list.length; i < len; i++){
       time = time_list[i];
-      time_type = time.match(/[wdhms]$/i);
+      type = time.match(/[wdhms]$/i);
 
-      if(time_type){
-         console.log(time_type);
-         time_value = Number(time.replace(time_type[0], ''));
+      if(type){
+         value = Number(time.replace(type[0], ''));
 
-         switch(time_type[0].toLowerCase()){
+         switch(type[0].toLowerCase()){
             case 'w':
-               summ += time_value * 604800000;
+               summ += value * 604800000;
             break;
             case 'd':
-               summ += time_value * 86400000;
+               summ += value * 86400000;
             break;
             case 'h':
-               summ += time_value * 3600000;
+               summ += value * 3600000;
             break;
             case 'm':
-               summ += time_value * 60000;
+               summ += value * 60000;
             break;
             case 's':
-               summ += time_value * 1000;
+               summ += value * 1000;
             break;
          }
       }else if(!isNaN(parseFloat(time)) && isFinite(time)){
